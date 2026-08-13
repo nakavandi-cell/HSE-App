f4d414307503544f9cd26b0d39904813cc5f7029
nakavandi-cell <nakavandi@gmail.com>
2026-08-13 17:05:50 +0330

    Create main.dart

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      appBar: AppBar(title: Text('HSE Checklist App')),
+      body: Center(child: Text('خوش آمدید - اپلیکیشن در حال آماده‌سازی است')),
+    ),
+  ));
+}
